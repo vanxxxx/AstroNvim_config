7efdeb2eff4e90e167aa24bd10acc4b5ed4acfab
vanx laptop <vanxxxyy@gmail.com>
2025-05-14 21:45:12 +0800

    new

---
+++ lua/community.lua
@@ -0,0 +1,15 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.recipes.neovide" },
+  -- debug
+  { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  -- import/override with your plugins folder
+}
